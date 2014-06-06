9ffbca5f5a3e066e14127cd1f068579203b3c21b
coderlfy <13403692778@126.com>
2014-06-06 23:41:54 +0800

    test send file

---
+++ DevIM/Test/TestSendFile.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace DevIM.Test
+{
+    partial class TestSendFile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // TestSendFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 152);
+            this.Name = "TestSendFile";
+            this.Text = "TestSendFile";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
