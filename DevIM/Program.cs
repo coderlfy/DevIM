3ae9b6cc17b4c13ad07017fc503b1cc561f0125d
lhzhanghui <445680507@qq.com>
2014-06-07 12:12:29 +0800

    comment

--- DevIM/Program.cs
+++ DevIM/Program.cs
@@ -13,6 +13,7 @@ namespace DevIM
         [STAThread]
         static void Main()
         {
+            //zh
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Logon());
