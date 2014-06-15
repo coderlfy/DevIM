c06708522025c221528210eb7adb435125d508aa
coderlfy <13403692778@126.com>
2014-06-15 11:44:14 +0800

    数据库访问层

---
+++ DevIMSqlLibrary/TUserClass.cs
@@ -0,0 +1,23 @@
+/****************************************
+***创建人：bhlfy
+***创建时间：2014-06-15 11:28:59
+***公司：山西博华科技有限公司
+***修改人：
+***修改时间：
+***文件描述：。
+*****************************************/
+
+using BusinessBase;
+using Fundation.Core;
+using System;
+
+namespace DevIMSqlLibrary
+{
+    public class TUserClass : GeneralAccessor
+    {
+        #region 自定义业务关联访问
+        #endregion
+    }
+}
+
+
