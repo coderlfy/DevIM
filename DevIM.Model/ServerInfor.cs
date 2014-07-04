bb156dca7ed33855100ed1b2db85dadaedc5afb5
coderlfy <13403692778@126.com>
2014-07-04 19:45:35 +0800

    git不稳定丢失

---
+++ DevIM.Model/ServerInfor.cs
@@ -0,0 +1,37 @@
+﻿using Fundation.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DevIM.Model
+{
+    public class ServerInfor
+    {
+
+        public const string KeyNameServerIP = "ServerIP";
+        public const string KeyNameServerPort = "ServerPort";
+        private static object _ip = "192.168.159.104";
+
+        public static object _Ip
+        {
+            get { return _ip; }
+            set { _ip = value; }
+        }
+
+
+        private static object _port = 1005;
+
+        public static object _Port
+        {
+            get { return _port; }
+            set { _port = value; }
+        }
+
+        public static void GetParamsFromConfig()
+        {
+            Config.Get(KeyNameServerPort, ref _port);
+            Config.Get(KeyNameServerIP, ref _ip);
+        }
+    }
+}
